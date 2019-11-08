d33efb48d3290b8a77f62faf58a3e956a1367cec
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2019-11-08 15:07:44 -0600

    refactor

--- upload.sh
+++ upload.sh
@@ -14,34 +14,10 @@ with open('$GITHUB_EVENT_PATH', 'r') as file:
 print(action)
 ")
 
-# Infer the twine repository url from the tag
-TWINE_REPOSITORY_URL=$(python -c "
-import re
-
-pattern = '(?P<version>v{n}.{n}.{n})(?P<suffix>.*)?'
-pattern = pattern.format(n='[0-9]+')
-pattern = re.compile(pattern)
-match = pattern.search('$tag')
-
-if match:
-    match = match.groupdict()
-    keys = ['version', 'suffix']
-    version, suffix = map(match.get, keys)
-    url = "https://{}.pypi.org/legacy/"
-
-    if version and not suffix:
-        print(url.format('upload'))
-
-    if version and suffix.startswith('.dev'):
-        print(url.format('test'))
-")
-
 echo
 echo "=================================================="
 echo "Release $tag was $action on GitHub"
 echo "=================================================="
-echo "TWINE_REPOSITORY_URL: $TWINE_REPOSITORY_URL"
-echo
 
 build_package() {
     # Remove build artifacts
@@ -74,9 +50,57 @@ upload_to_pypi() {
 
     # Upload to pypi or testpypi, overwrite if files already exist.
     python -m twine upload dist/* --skip-existing --verbose \
-        --username $PYPI_USERNAME --password $PYPI_PASSWORD \
-        --repository-url $TWINE_REPOSITORY_URL
+        --username $username --password $password \
+        --repository-url $repository_url
 }
 
 # If release was published on GitHub then upload to PyPI
-if [ $action = "published" ]; then upload_to_pypi; fi
+if [ $action = "published" ]
+then
+    # Infer whether to use PyPI (production) or Test PyPI (development).
+    repository=$(python -c "
+    import re
+
+    pattern = '(?P<version>v{n}.{n}.{n})(?P<suffix>.*)?'
+    pattern = pattern.format(n='[0-9]+')
+    pattern = re.compile(pattern)
+    match = pattern.search('$tag')
+
+    if match:
+        match = match.groupdict()
+        keys = ['version', 'suffix']
+        version, suffix = map(match.get, keys)
+
+        if version and not suffix:
+            print('production')
+
+        if version and suffix.startswith('.dev'):
+            print('development')
+    ")
+
+    # Raise error if unable to make the inference for PyPI or Test PyPI.
+    if [ -z $repository ]
+    then
+        echo "Unable to infer which PyPI repository to use."
+        exit 1
+    fi
+
+    # If production, set variables for PyPI.
+    if [ $repository = "production" ]
+    then
+        username=$PYPI_USERNAME
+        password=$PYPI_PASSWORD
+        repository_url="https://upload.pypi.org/legacy/"
+    fi
+
+    # If development, set variables for Test PyPI.
+    if [ $repository = "development" ]
+    then
+        username=$TEST_PYPI_USERNAME
+        password=$TEST_PYPI_PASSWORD
+        repository_url="https://test.pypi.org/legacy/"
+    fi
+
+    upload_to_pypi
+
+fi
