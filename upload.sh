10888b11adacd9153b48cba1a517a1b01ff0fc94
Jeff Hernandez <inboxjeff@outlook.com>
2019-11-08 14:04:50 -0600

    twine repo url

--- upload.sh
+++ upload.sh
@@ -6,28 +6,66 @@ tag=$(basename $GITHUB_REF)
 # Get action that triggered release event
 action=$(python -c "
 import json
+
 with open('$GITHUB_EVENT_PATH', 'r') as file:
     event = json.load(file)
     action = event.get('action')
+
 print(action)
 ")
 
-echo "Release $tag was $action on GitHub ..."
+# Infer the twine repository url from the tag
+TWINE_REPOSITORY_URL=$(python -c "
+import re
 
-upload_to_pypi () {
-    # Checkout specified tag
-    git checkout tags/$tag
+pattern = '(?P<version>v{n}.{n}.{n})(?P<suffix>.*)?'
+pattern = pattern.format(n='[0-9]+')
+pattern = re.compile(pattern)
+match = pattern.search('$tag')
 
+if match:
+    match = match.groupdict()
+    keys = ['version', 'suffix']
+    version, suffix = map(match.get, keys)
+    url = "https://{}.pypi.org/legacy/"
+
+    if version and not suffix:
+        print(url.format('upload'))
+
+    if version and suffix.startswith('.dev'):
+        print(url.format('test'))
+")
+
+echo
+echo "=================================================="
+echo "Release $tag was $action on GitHub"
+echo "=================================================="
+echo "TWINE_REPOSITORY_URL: $TWINE_REPOSITORY_URL"
+echo
+
+build_package() {
     # Remove build artifacts
-    rm -rf .eggs/ rm -rf dist/ rm -rf build/
+    artifacts=( ".eggs" "dist" "build" )
+    for artifact in "${artifacts[@]}"
+    do
+    if [ -d $artifact ]; then rm -rf $artifact; fi
+    done
 
     # Create distributions
     python setup.py -q sdist bdist_wheel
+}
+
+upload_to_pypi() {
+    # Checkout specified tag
+    git checkout tags/$tag
+
+    # Create pypi package
+    build_package
 
     # Create virtualenv to download twine
     python -m venv venv
     . venv/bin/activate
-    
+
     # Upgrade pip
     python -m pip install --upgrade pip -q
 
@@ -36,8 +74,8 @@ upload_to_pypi () {
 
     # Upload to pypi or testpypi, overwrite if files already exist.
     python -m twine upload dist/* --skip-existing --verbose \
-    --username $PYPI_USERNAME --password $PYPI_PASSWORD \
-    --repository-url $TWINE_REPOSITORY_URL
+        --username $PYPI_USERNAME --password $PYPI_PASSWORD \
+        --repository-url $TWINE_REPOSITORY_URL
 }
 
 # If release was published on GitHub then upload to PyPI
