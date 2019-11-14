eee6fe0cd1c88ccd56d93b925707e98174f32791
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2019-11-14 14:52:34 -0600

    URL Inference (#3)

--- README.md
+++ README.md
@@ -1,33 +1,81 @@
-# gh-action-pypi-upload
-Use this GitHub action to upload a Python package to PyPi
+# GitHub Action - PyPI Upload
 
-## Prereqs 
+Use this GitHub Action to upload a Python package to PyPI.
 
-* You can `pip install` the library locally and import it
+## Install
 
-## Usage 
+In your repository, add the following lines to `.github/workflows/release.yml`:
 
-Add the following lines to `.github/main.workflow`
+```yaml
+on:
+  release:
+    types: [published]
+
+name: Release
+jobs:
+  pypi:
+    name: Release to PyPI
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@master
+    - name: Upload to PyPI
+      uses: FeatureLabs/gh-action-pypi-upload@master
+      env:
+        PYPI_USERNAME: ${{ secrets.PYPI_USERNAME }}
+        PYPI_PASSWORD: ${{ secrets.PYPI_PASSWORD }}
+        TEST_PYPI_USERNAME: ${{ secrets.TEST_PYPI_USERNAME }}
+        TEST_PYPI_PASSWORD: ${{ secrets.TEST_PYPI_PASSWORD }}
+```
+
+Then, add the following secrets to the repository settings:
+  - `PYPI_USERNAME`
+  - `PYPI_PASSWORD`
+  - `TEST_PYPI_USERNAME`
+  - `TEST_PYPI_PASSWORD`
+
+## Usage
+
+The published release tag from GitHub will determine which repository the package is uploaded to. The tag schemes are based on the specifications in [PEP 440](https://www.python.org/dev/peps/pep-0440).
+
+### Upload to Test PyPI
+
+To upload a package to [Test PyPI](https://test.pypi.org/), the tag must follow the version schemes for [developmental releases](https://www.python.org/dev/peps/pep-0440/#developmental-releases).
+
+#### Developmental releases
 
 ```
-workflow "Release" {
-  on = "release"
-  resolves = ["PyPI"]
-}
-
-action "PyPI" {
-  uses = "FeatureLabs/gh-action-pypi-upload@master"
-  secrets = ["PYPI_USERNAME", "PYPI_PASSWORD"]
-  env = {
-    TWINE_REPOSITORY_URL = "https://test.pypi.org/legacy/"
-    }
-}
+X.Y.devN          # Developmental release
+X.YaN.devM        # Developmental release of an alpha release
+X.YbN.devM        # Developmental release of a beta release
+X.YrcN.devM       # Developmental release of a release candidate
+X.Y.postN.devM    # Developmental release of a post-release
 ```
 
-*  Update `PYPI_USERNAME`, `PYPI_PASSWORD` secrets in repo settings
+### Upload to PyPI
 
-*Note: Once you release a version of your package to PyPi you cannot rerelease that same version number, even if you delete it*
+To upload a package to [PyPI](https://pypi.org/), the tag can follow version schemes for [pre-releases](https://www.python.org/dev/peps/pep-0440/#pre-releases), [final releases](https://www.python.org/dev/peps/pep-0440/#final-releases), or [post-releases](https://www.python.org/dev/peps/pep-0440/#post-releases).
 
-* Make a test release and confirm the repo shows up on [TestPyPI](https://test.pypi.org/)
+#### Pre-releases
 
-* If it works, change `TWINE_REPOSITORY_URL = "https://upload.pypi.org/legacy/"` to point to production PyPi server
+```
+X.YaN     # Alpha release
+X.YbN     # Beta release
+X.YrcN    # Release Candidate
+```
+
+#### Final releases
+
+```
+X.Y     # Final release
+```
+
+#### Post-releases
+
+```
+X.Y.postN       # Post-release
+X.YaN.postM     # Post-release of an alpha release
+X.YbN.postM     # Post-release of a beta release
+X.YrcN.postM    # Post-release of a release candidate
+```
+
+*Note: Once you release a version of your package to PyPI, you cannot rerelease that same version number even if you delete it.*
--- upload.sh
+++ upload.sh
@@ -1,44 +1,101 @@
 #!/bin/sh
 
-# Get tag from environment
+# Get release tag from environment and checkout branch.
 tag=$(basename $GITHUB_REF)
+git checkout tags/$tag
 
-# Get action that triggered release event
+# Get package version.
+version=$(python setup.py --version)
+
+# Check if release tag matches the package version.
+pip install --quiet "packaging>=17.0"
+
+match=$(python -c "
+from packaging.version import parse
+
+match = parse('$tag') == parse('$version')
+print(match)
+")
+
+if [ $match != "True" ]; then
+    echo "Release $tag does not match package $version"
+    exit 1
+fi
+
+# Get action that triggered event.
 action=$(python -c "
 import json
+
 with open('$GITHUB_EVENT_PATH', 'r') as file:
     event = json.load(file)
     action = event.get('action')
+
 print(action)
 ")
 
-echo "Release $tag was $action on GitHub ..."
+# Infer which repository to use.
+repository=$(python -c "
+from packaging.version import parse, Version
 
-upload_to_pypi () {
-    # Checkout specified tag
-    git checkout tags/$tag
+version = parse('$tag')
+if isinstance(version, Version):
+    repository = 'Test PyPI' if version.is_devrelease else 'PyPI'
+    print(repository)
+")
 
-    # Remove build artifacts
+build_package() {
+    # Remove build artifacts.
     rm -rf .eggs/ rm -rf dist/ rm -rf build/
 
-    # Create distributions
-    python setup.py -q sdist bdist_wheel
-
-    # Create virtualenv to download twine
-    python -m venv venv
-    . venv/bin/activate
-    
-    # Upgrade pip
-    python -m pip install --upgrade pip -q
-
-    # Install twine, module used to upload to pypi
-    python -m pip install twine -q
-
-    # Upload to pypi or testpypi, overwrite if files already exist.
-    python -m twine upload dist/* --skip-existing --verbose \
-    --username $PYPI_USERNAME --password $PYPI_PASSWORD \
-    --repository-url $TWINE_REPOSITORY_URL
+    # Create distributions.
+    python setup.py --quiet sdist bdist_wheel
 }
 
-# If release was published on GitHub then upload to PyPI
-if [ $action = "published" ]; then upload_to_pypi; fi
+upload_package() {
+    # Build the package to upload.
+    build_package
+
+    # Create and activate the virtualenv to download twine.
+    python -m venv venv; . venv/bin/activate
+
+    # Upgrade pip.
+    python -m pip install --quiet --upgrade pip
+
+    # Install twine which is used to upload the package.
+    python -m pip --quiet install twine
+
+    echo
+    echo "----------  Uploading to $repository  --------------------"
+    echo
+
+    # Upload the package to PyPI or Test PyPI. Overwrite if files already exist.
+    python -m twine upload dist/* --skip-existing --verbose \
+       --username $1 --password $2 --repository-url $3
+}
+
+release_package() {
+    # If the inferred repository is PyPI, then release to PyPI.
+    if [ "$repository" = "PyPI" ]; then
+        TWINE_REPOSITORY_URL="https://upload.pypi.org/legacy/"
+        upload_package $PYPI_USERNAME $PYPI_PASSWORD $TWINE_REPOSITORY_URL
+
+    # Else if the inferred repository is Test PyPI, then release to Test PyPI.
+    elif [ "$repository" = "Test PyPI" ]; then
+        TWINE_REPOSITORY_URL="https://test.pypi.org/legacy/"
+        upload_package $TEST_PYPI_USERNAME $TEST_PYPI_PASSWORD $TWINE_REPOSITORY_URL
+
+    # Else, raise an error and exit.
+    else
+        echo "Unable to make inference on release $tag"
+        exit 1
+    fi
+}
+
+echo
+echo "=================================================="
+echo "Release $tag was $action on GitHub"
+echo "=================================================="
+echo
+
+# If release was published on GitHub then release package.
+if [ $action = "published" ]; then release_package; fi
